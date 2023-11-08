833cd427851ac2546d8ac1cce265c22f9527eb46
Kookiie88 <alexander@jj-media.se>
2023-11-09 01:06:52 +0700

    finished hamburger menu styling

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-const hamburgerButton = document.querySelector('.header__hamburgerButton');
-const hamburgerMenu = document.querySelector('.header__hamburgerMenu');
+const hamburgerMenuContainer = document.querySelector('.header__hamburgerMenuContainer');
+const hamburgerButton = document.querySelector('.header__hamburgerButtonOpen');
 const hamburgerCloseButton = document.querySelector('.header__hamburgerCloseButton');
-const logoAndCompanyName = document.querySelector('.header__divLogoAndCompanyName');
 const html = document.querySelector('.html');	
 
 hamburgerButton.addEventListener('click', () => {
-    hamburgerMenu.classList.add('header__hamburgerMenu--open');
+    hamburgerMenuContainer.classList.add('header__hamburgerMenuContainer--open');
     html.classList.add('html--overflowHidden');
 });
 
 hamburgerCloseButton.addEventListener('click', () => {
-    hamburgerMenu.classList.remove('header__hamburgerMenu--open');
+    hamburgerMenuContainer.classList.remove('header__hamburgerMenuContainer--open');
     html.classList.remove('html--overflowHidden');
 });
